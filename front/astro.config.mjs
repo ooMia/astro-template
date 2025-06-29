69fb3219bb128ca10e846d1600c1d6450491d268
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-06-29 11:15:55 +0900

    🛠️ Chore : latest compat

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://oomia.github.io/100_java_mistakes/",
-  base: "/100_java_mistakes",
+  site: "https://oomia.github.io/astro-template/",
+  base: "/astro-template",
   trailingSlash: "ignore",
 });
